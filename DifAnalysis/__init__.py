bb9cdd8177c063f5d0d4dec77c8cef92b1af384e
dapeng <757049042@qq.com>
2018-10-11 10:57:30 +0800

    del some

--- DifAnalysis/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-# @Author  : zhipeng zhao
-# @Date    : 2017/11/20
-# @File    : __init__.py.py
-import argparse
-
-argparse.ArgumentParser().add_argument()
--- DifAnalysis/conf/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-# @Author  : zhipeng zhao
-# @Date    : 2017/11/20
-# @File    : __init__.py.py
